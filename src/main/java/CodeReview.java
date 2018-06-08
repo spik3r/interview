a4b0f20301da0767140861bd0d29a4cd217090c1
Kai Tait <kai.tait@rewe-digital.com>
2018-06-08 14:04:26 +0200

    little improvements

--- src/main/java/CodeReview.java
+++ src/main/java/CodeReview.java
@@ -1,19 +1,47 @@
-public class CodeReview {
-    public Ball ball;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class CodeReview {
+    private static final Logger LOG = LoggerFactory.getLogger(CodeReview.class);
+    public Integer MAX_ENTITIES = 100;
+    public String APP_TITLE = "Printing " + MAX_ENTITIES + " Entities";
+    public Ball b;
+
+    private Integer getMAX_ENTITIES() {
+        return MAX_ENTITIES;
+    }
 
     public CodeReview() {
-        this.ball = new Ball();
+        this.b = new Ball();
     }
 
     public void doStuffWithBall() {
-        this.ball.diameter = 123;
-        this.ball.colour = "pink";
+        this.b.d = 123;
+        this.b.c = "pink";
 
     }
 
     public class Ball {
-        public int diameter = 5;
-        public String colour = "blue";
+        public int d = 5;
+        public String c = "blue";
+
+        public void playWithBall() {
+            System.out.println("Playing with ball");
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void print100Entities() {
+        String output= APP_TITLE + ": ";
+        for (int i=1; i<=500; i++) {
+            output = output + ", ";
+            output = output + i;
+        }
+        System.out.println(output);
     }
 }
